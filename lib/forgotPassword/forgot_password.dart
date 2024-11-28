5a8999a923428ca0e9f3403a73b364ce8c835bb8
Sanam Dongol <sanam@Sanams-MacBook-Pro.local>
2024-11-28 08:01:51 +0545

    Forgot password first page ui wip

--- lib/forgotPassword/forgot_password.dart
+++ lib/forgotPassword/forgot_password.dart
@@ -19,6 +19,34 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
           },
         ),
       ),
+      body: Column(
+        children: [
+          Text(
+            'WIDGET COMPANY',
+            style: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 33.0,
+                fontWeight: FontWeight.bold),
+          ),
+          Text('Get it done right away'),
+          SizedBox(height: 50.0),
+          Text(
+            'Reset Password',
+            style: TextStyle(fontSize: 20),
+          ),
+          Text(
+            ('Enter the email associated with your account and we will send you and email with instruction to reset your password.'),
+          ),
+          TextField(
+            decoration: InputDecoration(
+                labelText: "Email", hintText: 'Enter your email'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Continue'),
+          ),
+        ],
+      ),
     );
   }
 }
