4a87ed28168b201601b114625ff3a614b9b57d1b
Sanam Dongol <sanam@Sanams-MacBook-Pro.local>
2024-12-04 09:02:23 +0545

    Forgot  password wip

--- lib/forgotPassword/forgot_password.dart
+++ lib/forgotPassword/forgot_password.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cosmos_class/forgotPassword/forgot_second.dart';
 
 class ForgotPasswordPage extends StatefulWidget {
   const ForgotPasswordPage({super.key});
@@ -8,6 +9,8 @@ class ForgotPasswordPage extends StatefulWidget {
 }
 
 class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,33 +22,64 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
           },
         ),
       ),
-      body: Column(
-        children: [
-          Text(
-            'WIDGET COMPANY',
-            style: TextStyle(
-                color: Colors.blueAccent,
-                fontSize: 33.0,
-                fontWeight: FontWeight.bold),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Center(
+                child: Text(
+                  'WIDGET COMPANY',
+                  style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontSize: 33.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Center(child: Text('Get it done right away')),
+              SizedBox(height: 60.0),
+              Text(
+                'Reset Password',
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.blueAccent,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              Text(
+                ('Enter the email associated with your account and we will send you and email with instruction to reset your password.'),
+              ),
+              TextFormField(
+                validator: (userKoInput) {
+                  if (userKoInput == null || userKoInput.isEmpty) {
+                    return "Email khali garnu hunna";
+                  }
+                },
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    labelText: "Email", hintText: 'Enter your email'),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const ForgotSecondPage(),
+                      ),
+                    );
+                  }
+                },
+                child: Text('Continue'),
+              ),
+            ],
           ),
-          Text('Get it done right away'),
-          SizedBox(height: 50.0),
-          Text(
-            'Reset Password',
-            style: TextStyle(fontSize: 20),
-          ),
-          Text(
-            ('Enter the email associated with your account and we will send you and email with instruction to reset your password.'),
-          ),
-          TextField(
-            decoration: InputDecoration(
-                labelText: "Email", hintText: 'Enter your email'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Continue'),
-          ),
-        ],
+        ),
       ),
     );
   }
---
+++ lib/forgotPassword/forgot_second.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ForgotSecondPage extends StatefulWidget {
+  const ForgotSecondPage({super.key});
+
+  @override
+  State<ForgotSecondPage> createState() => _ForgotSecondPageState();
+}
+
+class _ForgotSecondPageState extends State<ForgotSecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
